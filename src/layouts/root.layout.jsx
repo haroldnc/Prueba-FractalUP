8e2ee39924419e3f13f920b5d8cee5adf1cda476
Harold Navarro <harold.mth95@gmail.com>
2023-03-30 16:51:03 -0500

    Add: NavBar template

--- src/layouts/root.layout.jsx
+++ src/layouts/root.layout.jsx
@@ -1,8 +1,18 @@
+import { Outlet } from 'react-router-dom'
 import MusicPlayer from '../components/MusicPlayer'
+import SearchBar from '../components/SearchBar'
 
 const RootLayout = () => {
   return (
     <>
+      <div className='flex w-screen justify-between gap-4'>
+        <section className='flex-1 p-4 sm:py-8'>
+          <SearchBar />
+          <div className='w-full'>
+            <Outlet />
+          </div>
+        </section>
+      </div>
       <MusicPlayer />
     </>
   )
